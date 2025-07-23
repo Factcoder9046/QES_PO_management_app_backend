2c83b193c9c7cb8692bcfd735beb5cdb4bf1a6a6
Zafeer <mohammadzafeer2610@gmail.com>
2025-07-23 15:08:15 +0530

    added estimated dispatch date to order model

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,41 +1,45 @@
 import mongoose from "mongoose";
 
-
-const orderSchema = new mongoose.Schema({
-  orderNumber: { type: String, required: true, unique: true },
-  clientName: { type: String, required: true },
-  companyName: { type: String },
-  gstNumber: { type: String },
-  contact: { type: String, required: true },
-  address: { type: String, required: true },
-  zipCode: { type: String, required: true },
-  products: [
-    {
-      name: { type: String, required: true },
-      price: { type: Number, required: true },
-      quantity: { type: Number, required: true, min: 0 },
-      remark:{type:String,required:false}
+const orderSchema = new mongoose.Schema(
+  {
+    orderNumber: { type: String, required: true, unique: true },
+    clientName: { type: String, required: true },
+    companyName: { type: String },
+    gstNumber: { type: String },
+    contact: { type: String, required: true },
+    address: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    products: [
+      {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true, min: 0 },
+        remark: { type: String, required: false },
+      },
+    ],
+    estimatedDispatchDate: { type: Date },
+    generatedBy: {
+      user: {
+        username: { type: String, required: true },
+      },
+      employeeId: { type: String, required: true },
     },
-  ],
-  estimatedDispatchDate: { type: Date },
-  generatedBy: {
-    user: {
+    orderThrougth: { type: String },
+    department: { type: String, required: true },
+    createdBy: {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
       username: { type: String, required: true },
     },
-    employeeId: { type: String, required: true },
-  },
-  orderThrougth: { type: String },
-  department: { type: String, required: true },
-  createdBy: {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    username: { type: String, required: true },
-  },
-  status: {
+    status: {
       type: String,
-      enum: ["pending", "processing", "completed", "delayed","rejected"],
+      enum: ["pending", "processing", "completed", "delayed", "rejected"],
       default: "pending",
     },
-   assignedToUser: {
+    assignedToUser: {
       userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
@@ -47,8 +51,10 @@ const orderSchema = new mongoose.Schema({
         required: false,
       },
     },
-  isdeleted: { type: Boolean, default: false },
-  deletedAt: { type: Date },
-}, { timestamps: true })
+    isdeleted: { type: Boolean, default: false },
+    deletedAt: { type: Date },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Order", orderSchema);
